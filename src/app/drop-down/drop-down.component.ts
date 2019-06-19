e68033160f92b2bf42cb40338227d014d3a60fbe
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-06-19 07:44:48 +0530

    drop down component add

--- src/app/drop-down/drop-down.component.ts
+++ src/app/drop-down/drop-down.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-drop-down',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drop-down.component.css']
 })
 export class DropDownComponent implements OnInit {
+  public dropdownList:object = [];
+
+  @Input('dropDownObject') set dropDownObject(v: object) {
+    console.log('Input')
+    this.dropdownList = v;
+  }
+  @Output() dropDownEmitter = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
+    console.log('ngOnInit')
   }
 
 }
