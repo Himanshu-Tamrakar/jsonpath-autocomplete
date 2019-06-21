72d3c3511cfef9b3afdf3e98418a25aca162c410
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-06-21 19:23:03 +0530

    app module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DropDownComponent } from './drop-down/drop-down.component';
 import { ObjectAutocompleteComponent } from './object-autocomplete/object-autocomplete.component';
+import { FilterPipe } from './core/pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     DropDownComponent,
-    ObjectAutocompleteComponent
+    ObjectAutocompleteComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
