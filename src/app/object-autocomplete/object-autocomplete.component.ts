2c99c64269d48ad365711985b9b6a8505559a797
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-06-24 15:21:47 +0530

    object auto complete

--- src/app/object-autocomplete/object-autocomplete.component.ts
+++ src/app/object-autocomplete/object-autocomplete.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import jp from 'jsonpath/jsonpath.min';
 import { Observable, of, Subject } from 'rxjs';
 import { debounceTime, delay, distinctUntilChanged, flatMap, map, tap } from 'rxjs/operators';
+import { SubscribalService } from '../core/services/subscribal.service';
 
 @Component({
   selector: 'app-object-autocomplete',
@@ -13,41 +14,54 @@ export class ObjectAutocompleteComponent {
   public suggestionArray: object = [];
   public searchText: string = '';
 
-  private jsonObject =
-    {
-      "product": "Live JSON generator",
-      "version": 3.1,
-      "releaseDate": "2014-06-25T00:00:00.000Z",
-      "demo": true,
-      "person": {
-        "id": 12345,
-        "name": "John Doe",
-        "phones": {
-          "home": "800-123-4567",
-          "mobile": "877-123-1234"
-        },
-        "email": [
-          "jd@example.com",
-          "jd@example.org"
-        ],
-        "dateOfBirth": "1980-01-02T00:00:00.000Z",
-        "registered": true,
-        "emergencyContacts": [
-          {
-            "name": "Jane Doe",
-            "phone": "888-555-1212",
-            "relationship": "spouse"
-          },
-          {
-            "name": "Justin Doe",
-            "phone": "877-123-1212",
-            "relationship": "parent"
-          }
-        ]
-      }
-    }
+  @ViewChild('inputBox') iB: ElementRef;
 
-  constructor() {
+
+  private jsonObject = {
+    "product": "Live JSON generator",
+    "version": 3.1,
+    "releaseDate": "2014-06-25T00:00:00.000Z",
+    "demo": true,
+    "hiyo": {
+      "0": {
+        "himanshu": "tamrakar"
+        },
+      "1": 'one'
+    },
+    "person": {
+      "id": 12345,
+      "name": "John Doe",
+      "phones": {
+        "home": "800-123-4567",
+        "mobile": "877-123-1234"
+      },
+      "email": [
+        "jd@example.com",
+        "jd@example.org"
+      ],
+      "dateOfBirth": "1980-01-02T00:00:00.000Z",
+      "registered": true,
+      "emergencyContacts": [
+        {
+          "name": "Jane Doe",
+          "phone": "888-555-1212",
+          "relationship": "spouse"
+        },
+        {
+          "name": "Justin Doe",
+          "phone": "877-123-1212",
+          "relationship": "parent"
+        }
+      ]
+    },
+    "test": { "0": "asd", "1": { "hilo": "World" }, "2": 'asda' },
+    "test1": ["asd", "sasd", "asda"]
+  }
+
+  constructor(private subscribalService:SubscribalService) {
+
+    this.subScripeToAppendKey('KEY_TO_APPEND');
+
     const subscription = this.keyUp.pipe(
       debounceTime(100),
       map(event => this.onKey(event))
@@ -61,27 +75,41 @@ export class ObjectAutocompleteComponent {
 
     try {
       if (typeof (jp.query(this.jsonObject, eventValue)[0]) == 'object') {
+        console.log('object', jp.query(this.jsonObject, eventValue))
         this.searchText = JSON.parse(JSON.stringify(''));
         this.suggestionArray = Object.keys(jp.query(this.jsonObject, eventValue)[0]);
       } else if (typeof (jp.query(this.jsonObject, eventValue)[0]) == 'string' || typeof (jp.query(this.jsonObject, eventValue)[0]) == 'number' || typeof (jp.query(this.jsonObject, eventValue)[0]) == 'boolean') {
+        console.log('other')
         this.searchText = JSON.parse(JSON.stringify(''));
         this.suggestionArray = [];
       } else if (typeof (jp.query(this.jsonObject, eventValue)[0]) == 'undefined') {
+        console.log('undefined')
         // this.suggestionArray = [];
         const valArr = eventValue.split('.');
         this.searchText = JSON.parse(JSON.stringify(valArr[valArr.length - 1]));
       } else {
+        console.log('else')
         this.searchText = JSON.parse(JSON.stringify(''));
         // this.suggestionArray = [];
       }
     } catch (err) {
+      console.log('catch')
       this.searchText = JSON.parse(JSON.stringify(''));
       // this.suggestionArray = [];
     }
   }
 
   public DropDownComponentEventListener(v: string): void {
-    console.log('emmited value', v);
+    let temp = this.iB.nativeElement.value.split('.');
+
+    this.iB.nativeElement.value = temp.join(".");
+    this.keyUp.next(this.iB.nativeElement.value);
+  }
+
+  public subScripeToAppendKey(key:string) {
+    this.subscribalService.returnSubjectKey(key).subscribe((value) => {
+
+    })
   }
 
 }
