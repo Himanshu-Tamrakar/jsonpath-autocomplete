448560582d2f5597f3e6e326798bddf7d90dfecd
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-06-25 11:43:08 +0530

    subsribal service add

--- src/app/core/services/subscribal.service.ts
+++ src/app/core/services/subscribal.service.ts
@@ -8,22 +8,30 @@ import { debounceTime, delay, distinctUntilChanged, flatMap, map, tap } from 'rx
 })
 export class SubscribalService {
   private keyToAppend = new Subject<any>();
-
+  public keyUp = new Subject<string>();
   constructor() { }
 
   public returnSubjectKey(keyType) {
-    switch(keyType) {
-      case 'KEY_TO_APPEND' : {
+    switch (keyType) {
+      case 'KEY_TO_APPEND': {
         return this.keyToAppend;
+      }
+      case 'KEY_UP': {
+        return this.keyUp;
       }
     }
   }
 
   public publishValue(keyType, value) {
-    switch(keyType) {
-      case 'KEY_TO_APPEND' : {
-          this.keyToAppend.next(value);
-          break;
+    debugger
+    switch (keyType) {
+      case 'KEY_TO_APPEND': {
+        this.keyToAppend.next(value);
+        break;
+      }
+      case 'KEY_UP': {
+        this.keyUp.next(value);
+        break;
       }
     }
   }
