d12d0b46bde81b78775cee37624bec8d9e50e911
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-06-29 10:40:20 +0530

    drop down component update, index subsribe and array check(from somewhere it is giving 1 value for dropdown listneed to check)

--- src/app/drop-down/drop-down.component.ts
+++ src/app/drop-down/drop-down.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { SubscribalService } from '../core/services/subscribal.service';
+import { Observable, of, Subject } from 'rxjs';
+import { debounceTime, delay, distinctUntilChanged, flatMap, map, tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-drop-down',
@@ -7,24 +10,36 @@ import { SubscribalService } from '../core/services/subscribal.service';
   styleUrls: ['./drop-down.component.css']
 })
 export class DropDownComponent implements OnInit {
-  public dropdownList:object = [];
+  public dropdownList:any = [];
   public sText:string = '';
+  public selectedIndex=0;
   @Input('searchText') set searchText(v: string) {
-    console.log("got", v)
     this.sText = v;
   }
 
-  @Input('dropDownObject') set dropDownObject(v: object) {
-    this.dropdownList = v;
-  }
+  // @Input('dropDownObject') set dropDownObject(v: object) {
+  //   this.dropdownList = v;
+  // }
   @Output() dropDownEmitter = new EventEmitter<any>();
 
-  constructor(public subscribalService:SubscribalService) { }
+  constructor(public subscribalService:SubscribalService) {
+    this.subscribalService.returnSubjectKey('ITEM_INDEX').subscribe((value) => {
+      this.selectedIndex = value;
+    });
+
+    this.subscribalService.returnSubjectKey('SUGGESTED_ARRAY').subscribe((obj) => {
+      if(Array.isArray(obj)) this.dropdownList = obj;
+    })
+  }
 
   ngOnInit() {}
 
   emit(value:any) {
     this.dropDownEmitter.emit(value);
   }
+
+  // onItemClick(value) {
+  //   this.subscribalService.publishValue('KEY_TO_APPEND',value);
+  // }
 
 }
